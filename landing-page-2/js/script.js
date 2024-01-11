0c60b5c45f7b6e79fa8bba93f2d94b26c6112034
Brunoake <126792752+Brunoake@users.noreply.github.com>
2024-01-11 03:01:46 -0300

    terminando java script

--- landing-page-2/js/script.js
+++ landing-page-2/js/script.js
@@ -14,3 +14,29 @@ function moveToFront(clickedPlan) {
     
     clickedPlan.classList.add('centered');
   }
+
+//   scroll
+function scrollToElement(elementSelector, instance = 0) {
+    const elements = document.querySelectorAll(elementSelector);
+    if(elements.length > instance) {
+
+        elements[instance].scrollIntoView({behavior: 'smooth'});
+    }
+}
+
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () => {
+    scrollToElement('#download-container');
+})
+
+link2.addEventListener('click', () => {
+    scrollToElement('#features-container');
+})
+
+link3.addEventListener('click', () => {
+    scrollToElement('#company');
+})
